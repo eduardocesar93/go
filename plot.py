aee09a82751200c4d53176a4ef543f80f9ee277e
eduardocesar93 <eduardocesar93@hotmail.com>
2017-02-04 13:47:01 -0200

    Fix bug in line plot

--- plot.py
+++ plot.py
@@ -22,7 +22,7 @@ def line_plot(label, data, directory, output_name, x_label, y_label, width=11.7,
     plt.grid(True)
     if linear_regression:
         slope, intercept, r_value, p_value, std_err = stats.linregress(data[0], data[1])
-        data_linear_regression = [slope * data[0][i] + intercept for i in range(data[0])]
+        data_linear_regression = [slope * data[0][i] + intercept for i in range(len(data[0]))]
         plt.plot(data[0], data_linear_regression, color='r', label='Linear Regression')
         plt.text(0.8, 0.8, 'Linear Regression: {0:.2f}x + {0:.2f}'.format(round(slope, 4)) + '\n'
                  + 'r-value: {0}'.format(round(r_value, 4)) + '\n'
